c7748c0a10a87f79ba8dae01bfb0a93e8efff3a7
edelweiss611428 <edelweiss611428@gmail.com>
2026-08-22 11:43:57 +1000

    Separate histograms for categorical and numerical PFASs

--- R/eda.R
+++ R/eda.R
@@ -9,8 +9,27 @@ library("tidyverse")
 pfas = read_xpt("data/P_PFAS.xpt")     # Perfluoroalkyl & polyfluoroalkyl substances
 
 
+# Categorical PFAS variables
+pfas_cat_vars = c(
+  "LBDBFOAL",
+  "LBDMFOSL",
+  "LBDMPAHL",
+  "LBDNFOAL",
+  "LBDNFOSL",
+  "LBDPFDEL",
+  "LBDPFHSL",
+  "LBDPFNAL",
+  "LBDPFUAL"
+)
+
+
+## ------ PFAS concentration distributions ------
+
 pfas_long = pfas %>%
-  select(-SEQN) %>%
+  select(
+    -SEQN,
+    -all_of(pfas_cat_vars)
+  ) %>%
   pivot_longer(
     cols = everything(),
     names_to = "PFAS",
@@ -29,9 +48,29 @@ ggplot(pfas_long, aes(x = concentration)) +
   ) +
   theme_bw()
 
-# Anything with "-Comment Code" is categorical (possibly binary)
 
-# Correlation structure of PFAS biomarkers
+## ------ Categorical PFAS distributions ------
+
+pfas_cat_long = pfas %>%
+  select(all_of(pfas_cat_vars)) %>%
+  pivot_longer(
+    cols = everything(),
+    names_to = "PFAS",
+    values_to = "category"
+  ) %>%
+  filter(!is.na(category))
+
+ggplot(pfas_cat_long, aes(x = factor(category))) +
+  geom_bar() +
+  facet_wrap(~ PFAS, scales = "free") +
+  labs(
+    title = "Distribution of Categorical PFAS Variables",
+    x = "Category",
+    y = "Count"
+  ) +
+  theme_bw()
+
+# ------ Correlation structure of PFAS biomarkers ------
 
 
 cor(pfas[,-1], use = "pairwise.complete.obs") %>% heatmap()
